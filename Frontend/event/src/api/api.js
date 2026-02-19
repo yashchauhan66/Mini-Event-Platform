9c8e46afebed3e185631983baea41e54b958e916
Your Name <your.email@example.com>
2026-02-19 23:12:47 +0530

    add aws url

--- Frontend/event/src/api/api.js
+++ Frontend/event/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "http://35.154.211.147:5000/api"
 });
 
 API.interceptors.request.use((req) => {
